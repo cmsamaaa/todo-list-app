7d8af5dff7e57ca0114443b583dd3ee5ef48041e
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-01-05 00:04:10 +0800

    add menu width & update colour to tailwind theme

--- frontend-react/tailwind.config.js
+++ frontend-react/tailwind.config.js
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
     extend: {
+      width: {
+        'menu': '18rem',
+      },
       backgroundImage: {
         'day': "url('/img/bg/pexels-nick-kwan-2614818.jpg')"
       },
@@ -16,10 +19,12 @@ export default {
         primary: '#202225',
         secondary: '#5865f2',
         gray: {
-          900: '#202225',
-          800: '#2f3136',
-          700: '#36393f',
-          600: '#4f545c',
+          900: '#191919',
+          850: '#202225',
+          800: '#242424',
+          700: '#2f3136',
+          600: '#36393f',
+          500: '#4f545c',
           400: '#d4d7dc',
           300: '#e3e5e8',
           200: '#ebedef',
