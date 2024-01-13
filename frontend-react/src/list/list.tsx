4af2485093293719096cfb0c4b6179d17b83897c
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-01-14 01:06:13 +0800

    add calendar icon to empty card

--- frontend-react/src/list/list.tsx
+++ frontend-react/src/list/list.tsx
@@ -5,7 +5,14 @@ const List = () => {
     <div className='list-container'>
       <div className='overlay'></div>
       <div className='list-empty-card'>
-        <div className='icon'>CALENDAR IMAGE</div>
+        <div className='icon'>
+          <img
+            className='calendar-icon'
+            src='/img/icon/Vexels-Office-Calendar.1024.png'
+            height={20}
+            alt='Calendar Icon'
+          />
+        </div>
         <div className='title'>Focus on your day</div>
         <div className='subtitle'>
           Get things done with My Day, a list that refreshes every day.
