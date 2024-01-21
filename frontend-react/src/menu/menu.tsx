f20bad97ef3923bc5d0ea2c763e6172de97deb12
Loh Chun Mun <102757707+cmsamaaa@users.noreply.github.com>
2024-01-21 21:28:35 +0800

    Add dynamic rendering of menu items based on data

--- frontend-react/src/menu/menu.tsx
+++ frontend-react/src/menu/menu.tsx
@@ -1,77 +1,83 @@
 import './menu.css';
 
+import { useEffect, useState } from 'react';
+import { loadList } from '../util/list.util';
+import { ListSection } from '../model/json.model';
+
 import { IoSunnyOutline } from 'react-icons/io5';
 import { FaRegStar } from 'react-icons/fa';
 import { TbColumns3 } from 'react-icons/tb';
 import { FaRegUser } from 'react-icons/fa6';
 import { GoHome } from 'react-icons/go';
 import { RxHamburgerMenu } from 'react-icons/rx';
+
 import MenuItem from './menu-item';
 import MenuBreak from './menu-break';
 import NewList from './new-list';
+import { IconType } from 'react-icons';
 
 const Menu = () => {
+  const [lists, setLists] = useState<ListSection>({});
+
+  const setIcon = (iconType: string): IconType => {
+    switch (iconType) {
+      case 'IoSunnyOutline':
+        return IoSunnyOutline;
+      case 'FaRegStar':
+        return FaRegStar;
+      case 'TbColumns3':
+        return TbColumns3;
+      case 'FaRegUser':
+        return FaRegUser;
+      case 'GoHome':
+        return GoHome;
+      case 'RxHamburgerMenu':
+        return RxHamburgerMenu;
+      default:
+        return RxHamburgerMenu;
+    }
+  };
+
+  useEffect(() => {
+    loadList()
+      .then((data: ListSection) => {
+        setLists(data);
+      })
+      .catch((err) => {
+        console.log('Fail to load list: ', err);
+      });
+  }, []);
+
   return (
     <>
       <div className='flex align-middle'>
         <div className='menu maximized'>
-          <MenuItem
-            iconType={IoSunnyOutline}
-            iconSize={20}
-            id={0}
-            label={'My Day'}
-            count={0}
-            first={true}
-          />
-          <MenuItem
-            iconType={FaRegStar}
-            iconSize={20}
-            id={1}
-            label={'Important'}
-            count={0}
-            first={false}
-          />
-          <MenuItem
-            iconType={TbColumns3}
-            iconSize={20}
-            id={2}
-            label={'Planned'}
-            count={0}
-            first={false}
-          />
-          <MenuItem
-            iconType={FaRegUser}
-            iconSize={20}
-            id={3}
-            label={'Assigned to me'}
-            count={0}
-            first={false}
-          />
-          <MenuItem
-            iconType={GoHome}
-            iconSize={20}
-            id={4}
-            label={'Tasks'}
-            count={0}
-            first={false}
-          />
+          {lists?.default &&
+            Object.entries(lists?.default).map(([key, value], idx) => (
+              <MenuItem
+                key={key}
+                iconType={setIcon(value)}
+                iconSize={20}
+                id={idx}
+                label={key}
+                count={0}
+                first={true}
+              />
+            ))}
           <MenuBreak />
-          <MenuItem
-            iconType={RxHamburgerMenu}
-            iconSize={20}
-            id={5}
-            label={'Getting started'}
-            count={7}
-            first={false}
-          />
-          <MenuItem
-            iconType={RxHamburgerMenu}
-            iconSize={20}
-            id={6}
-            label={'Groceries'}
-            count={5}
-            first={false}
-          />
+          {lists?.default &&
+            lists?.user &&
+            Object.entries(lists?.user).map(([key, value], idx) => (
+              <MenuItem
+                key={key}
+                iconType={setIcon(value)}
+                iconSize={20}
+                id={idx + Object.keys(lists.default).length}
+                label={key}
+                count={0}
+                first={true}
+              />
+            ))}
           <NewList />
         </div>
       </div>
